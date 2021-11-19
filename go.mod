7057a7df11e679b26e59599687855efb8db8d5f2
atyma <a.tymkiv99@gmail.com>
2021-11-19 15:18:59 +0200

    Loading configs from secretManager

--- go.mod
+++ go.mod
@@ -3,16 +3,24 @@ module github.com/uncut-fm/uncut-common
 go 1.17
 
 require (
+	cloud.google.com/go/secretmanager v1.0.0
 	entgo.io/ent v0.9.1
 	github.com/go-pg/pg/v10 v10.10.6
 	github.com/jackc/pgx/v4 v4.13.0
 	github.com/lib/pq v1.10.4
 	github.com/sirupsen/logrus v1.8.1
+	google.golang.org/genproto v0.0.0-20210921142501-181ce0d877f6
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
 
 require (
+	cloud.google.com/go v0.94.1 // indirect
 	github.com/go-pg/zerochecker v0.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.10.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -28,8 +36,15 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
 	github.com/vmihailenco/tagparser v0.1.2 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
 	golang.org/x/sys v0.0.0-20210923061019-b8560ed6a9b7 // indirect
 	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/api v0.57.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	mellium.im/sasl v0.2.1 // indirect
 )
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,12 +1,70 @@
 package config
 
+import (
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"context"
+	"fmt"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	CommonSecretEnvVar     = "COMMON_SECRET_ID"
+	ManagementSecretEnvVar = "MANAGEMENT_SECRET_ID"
+)
+
+type Configuration struct {
+	Common     Common            `yaml:"common"`
+	Management ManagementConfigs `yaml:"management"`
+}
+
+type Common struct {
+	ProjectID   string `yaml:"project_id"`
+	Environment string `yaml:"environment"`
+}
+
+type ManagementConfigs struct {
+	PlatformConfigs DBConfigs `yaml:"platform_configs"`
+}
+
 type DBConfigs struct {
-	Host                  string `json:"host"`
-	Port                  string `json:"port"`
-	DBName                string `json:"db_name"`
-	User                  string `json:"user"`
-	Password              string `json:"password"`
+	Host                  string `yaml:"host"`
+	Port                  string `yaml:"port"`
+	DBName                string `yaml:"db_name"`
+	User                  string `yaml:"user"`
+	Password              string `yaml:"password"`
 	MaxOpenConnections    int    `yaml:"max_open_conns"`
 	MaxIdleConnections    int    `yaml:"max_idle_conns"`
 	ConnectionMaxLifetime string `yaml:"conn_max_lifetime"`
+}
+
+func LoadConfigsFromSecretManager(ctx context.Context, client *secretmanager.Client, secretName string, dstStruct interface{}) error {
+	return loadConfigFromSecretManager(ctx, client, secretName, dstStruct)
+}
+
+func loadConfigFromSecretManager(ctx context.Context, client *secretmanager.Client, name string, dstStruct interface{}) error {
+	secretData, err := getSecretFromSecretManager(ctx, client, name)
+	if err != nil {
+		return err
+	}
+
+	return unmarshallYamlIntoStruct(secretData, dstStruct)
+}
+
+func getSecretFromSecretManager(ctx context.Context, client *secretmanager.Client, name string) ([]byte, error) {
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: name,
+	}
+
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to access secret version: %v", err)
+	}
+
+	return result.Payload.Data, nil
+}
+
+func unmarshallYamlIntoStruct(data []byte, dstStruct interface{}) error {
+	err := yaml.Unmarshal(data, dstStruct)
+	return err
 }
