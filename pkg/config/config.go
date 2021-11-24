448ebd231fb9c8027d53318962e49c61343d4141
atyma <a.tymkiv99@gmail.com>
2021-11-24 11:03:54 +0200

    Added admin token for backoffice configs

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -36,6 +36,7 @@ type BackofficeConfigs struct {
 	PlatformDB   DBConfigs     `yaml:"platform_db"`
 	Server       ServerConfigs `yaml:"server"`
 	SearchAPIUrl string        `yaml:"search_api_url"`
+	AdminToken   string        `yaml:"admin_token"`
 }
 
 type ServerConfigs struct {
