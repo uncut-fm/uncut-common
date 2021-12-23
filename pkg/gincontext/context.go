5ac547991b80cd7897d0316b3904d89536a95b09
atyma <a.tymkiv99@gmail.com>
2021-12-23 13:40:35 +0200

    Added context utils

---
+++ pkg/gincontext/context.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+type ContextKey = string
+
+const (
+	GinContextKey ContextKey = "GIN_CONTEXT_KEY"
+)
+
+func CreateWithGinContext(ctx context.Context, gin *gin.Context) context.Context {
+	return context.WithValue(ctx, GinContextKey, gin)
+}
+
+func GetGinContextFromContext(ctx context.Context) (*gin.Context, error) {
+	ginContext := ctx.Value(GinContextKey)
+
+	if ginContext == nil {
+		err := fmt.Errorf("could not retrieve gin.Context")
+		return nil, err
+	}
+
+	gc, ok := ginContext.(*gin.Context)
+	if !ok {
+		err := fmt.Errorf("gin.Context has wrong type")
+		return nil, err
+	}
+
+	return gc, nil
+}
