7203cc3a221766085f0b9e31f167827ee0154101
atyma <a.tymkiv99@gmail.com>
2021-12-29 16:14:41 +0200

    changed user model

--- model/user.go
+++ model/user.go
@@ -1,7 +1,8 @@
 package model
 
 type User struct {
-	Id                  int    `json:"user_id"`
+	ID                  int    `json:"id"`      // used to parse from ent model
+	UserId              int    `json:"user_id"` // used to parse from jwt token
 	Name                string `json:"name,omitempty"`
 	Email               string `json:"email"`
 	ProfileImageUrl     string `json:"profile_image_url,omitempty"`
