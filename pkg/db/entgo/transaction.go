70ed5e45580fc7d9b0f597b5a10916582c162325
atyma <a.tymkiv99@gmail.com>
2022-01-11 16:14:24 +0200

    added db transaction utils

---
+++ pkg/db/entgo/transaction.go
@@ -0,0 +1,23 @@
+package entgo
+
+import (
+	"fmt"
+	"github.com/uncut-fm/uncut-auth-api/ent"
+)
+
+func CommitOrRollback(tx *ent.Tx, err error) error {
+	if err != nil {
+		return rollback(tx, err)
+	}
+
+	return tx.Commit()
+}
+
+// rollback calls to tx.Rollback and wraps the given error
+// with the rollback error if occurred.
+func rollback(tx *ent.Tx, err error) error {
+	if rerr := tx.Rollback(); rerr != nil {
+		err = fmt.Errorf("%w: %v", err, rerr)
+	}
+	return err
+}
