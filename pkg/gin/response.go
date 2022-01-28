d9318afb0fdc383a0804f7f38644972ebd0e20d9
atyma <a.tymkiv99@gmail.com>
2022-01-28 18:54:24 +0200

    sending errors to sentry in gin response error func

--- pkg/gin/response.go
+++ pkg/gin/response.go
@@ -1,9 +1,16 @@
 package gin
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/getsentry/sentry-go"
+	"github.com/gin-gonic/gin"
+)
 
 func SetResponseError(ctx *gin.Context, statusCode int, err error) {
 	ctx.JSON(statusCode, gin.H{"message": err.Error()})
 	ctx.Abort()
+	if hub := sentry.GetHubFromContext(ctx); hub != nil {
+		hub.CaptureException(err)
+	}
+
 	return
 }
