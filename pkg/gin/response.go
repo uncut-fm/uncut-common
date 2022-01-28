0a6c86cd8651979428281bd5d4d6530b8a4ccf4b
atyma <a.tymkiv99@gmail.com>
2022-01-28 18:56:55 +0200

    fixed retrieving sentry hub from gin context

--- pkg/gin/response.go
+++ pkg/gin/response.go
@@ -1,14 +1,14 @@
 package gin
 
 import (
-	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
 )
 
 func SetResponseError(ctx *gin.Context, statusCode int, err error) {
 	ctx.JSON(statusCode, gin.H{"message": err.Error()})
 	ctx.Abort()
-	if hub := sentry.GetHubFromContext(ctx); hub != nil {
+	if hub := sentrygin.GetHubFromContext(ctx); hub != nil {
 		hub.CaptureException(err)
 	}
 
