a8b508e1983ae0964d39f68ac13df8a9cad854ea
atyma <a.tymkiv99@gmail.com>
2022-04-06 18:59:55 +0300

    added utils for paths in storage for spaces

--- pkg/storage/utils.go
+++ pkg/storage/utils.go
@@ -4,6 +4,7 @@ import "fmt"
 
 const (
 	momentsLocationPath = "%v/moments/%v/%v"                     // "{environment}/moments/{moment_id}/{file_name}"
+	spaceLocationPath   = "%v/space/%v/%v"                       // "{environment}/spaces/{space_id}/{file_name}"
 	publicFileFormat    = "https://storage.googleapis.com/%v/%v" // https://storage.googleapis.com/BUCKET_NAME/FILE_NAME
 )
 
@@ -13,4 +14,8 @@ func GetPublicFilePath(bucket, fileName string) string {
 
 func GetMomentsLocationPath(env, fileName string, momentID int) string {
 	return fmt.Sprintf(momentsLocationPath, env, momentID, fileName)
+}
+
+func GetSpaceLocationPath(env, fileName string, spaceID int) string {
+	return fmt.Sprintf(spaceLocationPath, env, spaceID, fileName)
 }
