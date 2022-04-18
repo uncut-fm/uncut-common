cba0bcd611945c60454a69f3a46cbf57d9606bed
atyma <a.tymkiv99@gmail.com>
2022-04-18 16:08:24 +0300

    added utils for paths in storage for users

--- pkg/storage/utils.go
+++ pkg/storage/utils.go
@@ -3,10 +3,12 @@ package storage
 import "fmt"
 
 const (
-	momentsLocationPath = "%v/moments/%v/%v"                     // "{environment}/moments/{moment_id}/{file_name}"
-	spaceLocationPath   = "%v/space/%v/%v"                       // "{environment}/space/{space_id}/{file_name}"
-	nftLocationPath     = "%v/nfts/%v/%v"                        // "{environment}/nfts/{nft_id}/{file_name}"
-	publicFileFormat    = "https://storage.googleapis.com/%v/%v" // https://storage.googleapis.com/BUCKET_NAME/FILE_NAME
+	momentsLocationPath = "%v/moments/%v/%v" // "{environment}/moments/{moment_id}/{file_name}"
+	spaceLocationPath   = "%v/space/%v/%v"   // "{environment}/space/{space_id}/{file_name}"
+	nftLocationPath     = "%v/nfts/%v/%v"    // "{environment}/nfts/{nft_id}/{file_name}"
+	userLocationPath    = "%v/users/%v/%v"   // "{environment}/users/{user_id}/{file_name}"
+
+	publicFileFormat = "https://storage.googleapis.com/%v/%v" // https://storage.googleapis.com/BUCKET_NAME/FILE_NAME
 )
 
 func GetPublicFilePath(bucket, fileName string) string {
@@ -23,4 +25,8 @@ func GetSpaceLocationPath(env, fileName string, spaceID int) string {
 
 func GetNftLocationPath(env, fileName string, nftID int) string {
 	return fmt.Sprintf(nftLocationPath, env, nftID, fileName)
+}
+
+func GetUserLocationPath(env, fileName string, userID int) string {
+	return fmt.Sprintf(userLocationPath, env, userID, fileName)
 }
