ebed7b1a828792dd766b6161a8ff050ad1f541b5
atyma <a.tymkiv99@gmail.com>
2022-06-07 15:12:03 +0300

    fixed auth client

--- pkg/auth/update.go
+++ pkg/auth/update.go
@@ -4,36 +4,32 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-resty/resty/v2"
-	"github.com/uncut-fm/uncut-backoffice-api/graph/model"
-	model_common "github.com/uncut-fm/uncut-common/model"
-	mngmt_model "github.com/uncut-fm/uncut-management-api-2/graph/model"
+	"github.com/uncut-fm/uncut-common/model"
 )
 
-func (a API) UpdateUser(input *model.UpdateUserAuthRequest) (*mngmt_model.User, error) {
+func (a API) UpdateUser(input *UpdateUserAuthRequest) (*model.User, error) {
 	commonUser, err := a.updateUser(input)
 	if a.log.CheckError(err, a.UpdateUser) != nil {
 		return nil, err
 	}
 
-	user := mngmt_model.NewUserFromCommonModel(commonUser)
-
-	return user, nil
+	return commonUser, nil
 }
 
-func (a API) updateUser(input *model.UpdateUserAuthRequest) (*model_common.User, error) {
+func (a API) updateUser(input *UpdateUserAuthRequest) (*model.User, error) {
 	response, err := a.makeUpdateUserRequest(input)
 	if a.log.CheckError(err, a.updateUser) != nil {
 		return nil, err
 	}
 
-	user := new(model_common.User)
+	user := new(model.User)
 
 	err = json.Unmarshal(response.Body(), user)
 
 	return user, a.log.CheckError(err, a.updateUser)
 }
 
-func (a API) makeUpdateUserRequest(input *model.UpdateUserAuthRequest) (*resty.Response, error) {
+func (a API) makeUpdateUserRequest(input *UpdateUserAuthRequest) (*resty.Response, error) {
 	resp, err := a.restyClient.R().EnableTrace().
 		SetBody(input).
 		SetHeader("admin-token", a.authAdminToken).
