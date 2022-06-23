b99bd1244cc155b89d5fe4994a1b885307e97bbc
atyma <a.tymkiv99@gmail.com>
2022-06-23 17:35:07 +0300

    added EtherToWei util

--- model/util.go
+++ model/util.go
@@ -1,7 +1,11 @@
 package model
 
 import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/vincent-petithory/dataurl"
+	"math/big"
+	"strings"
 )
 
 func IsIntFieldNew(newField *int, oldField int) bool {
@@ -32,4 +36,23 @@ func GetBytesFromDataURL(dataURLString string) ([]byte, error) {
 	}
 
 	return dataURL.Data, nil
+}
+
+// ParseBigFloat parse string value to big.Float
+func ParseBigFloat(value string) (*big.Float, error) {
+	f := new(big.Float)
+	f.SetPrec(236) //  IEEE 754 octuple-precision binary floating-point format: binary256
+	f.SetMode(big.ToNearestEven)
+	_, err := fmt.Sscan(value, f)
+	return f, err
+}
+
+func EtherToWei(eth *big.Float) *big.Int {
+	truncInt, _ := eth.Int(nil)
+	truncInt = new(big.Int).Mul(truncInt, big.NewInt(params.Ether))
+	fracStr := strings.Split(fmt.Sprintf("%.18f", eth), ".")[1]
+	fracStr += strings.Repeat("0", 18-len(fracStr))
+	fracInt, _ := new(big.Int).SetString(fracStr, 10)
+	wei := new(big.Int).Add(truncInt, fracInt)
+	return wei
 }
