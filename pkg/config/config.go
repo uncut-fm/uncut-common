01d2afa0a3bf803c7ffc55b1164e09877daa1969
atyma <a.tymkiv99@gmail.com>
2022-06-24 10:03:41 +0300

    added GetContractAddressBySymbol util for currencies

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,15 +1,5 @@
 package config
 
-import (
-	secretmanager "cloud.google.com/go/secretmanager/apiv1"
-	"context"
-	"errors"
-	"fmt"
-	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
-	"gopkg.in/yaml.v3"
-	"os"
-)
-
 const (
 	CommonSecretEnvVar     = "COMMON_SECRET_ID"
 	ManagementSecretEnvVar = "MANAGEMENT_SECRET_ID"
@@ -115,6 +105,20 @@ type Web3EventsConfigs struct {
 type Web3Currencies struct {
 	Weth  Web3CurrencyDetails `yaml:"weth"`
 	Cdols Web3CurrencyDetails `yaml:"cdols"`
+	Usdc  Web3CurrencyDetails `yaml:"usdc"`
+}
+
+func (w Web3Currencies) GetContractAddressBySymbol(symbol string) string {
+	switch symbol {
+	case w.Weth.Symbol:
+		return w.Weth.ContractAddress
+	case w.Cdols.Symbol:
+		return w.Cdols.ContractAddress
+	case w.Usdc.Symbol:
+		return w.Cdols.ContractAddress
+	default:
+		return ""
+	}
 }
 
 type Web3CurrencyDetails struct {
@@ -182,59 +186,4 @@ type OauthProviders struct {
 type SentryConfigs struct {
 	DSN        string  `yaml:"dsn"`
 	SampleRate float32 `yaml:"sample_rate"`
-}
-
-func LoadConfigsFromSecretManager(ctx context.Context, client *secretmanager.Client, configStruct interface{}) error {
-	secretName, err := getSecretNameByConfigStruct(configStruct)
-	if err != nil {
-		return err
-	}
-
-	return loadConfigFromSecretManager(ctx, client, secretName, configStruct)
-}
-
-func getSecretNameByConfigStruct(configStruct interface{}) (string, error) {
-	switch configStruct.(type) {
-	case *Common:
-		return os.Getenv(CommonSecretEnvVar), nil
-	case *ManagementConfigs:
-		return os.Getenv(ManagementSecretEnvVar), nil
-	case *BackofficeConfigs:
-		return os.Getenv(BackofficeSecretEnvVar), nil
-	case *AuthConfigs:
-		return os.Getenv(AuthSecretEnvVar), nil
-	case *TranscoderConfigs:
-		return os.Getenv(TranscoderSecretEnvVar), nil
-	case *Web3EventsConfigs:
-		return os.Getenv(Web3EventsSecretEnvVar), nil
-	default:
-		return "", errors.New("unsupported configStruct")
-	}
-}
-
-func loadConfigFromSecretManager(ctx context.Context, client *secretmanager.Client, name string, configStruct interface{}) error {
-	secretData, err := getSecretFromSecretManager(ctx, client, name)
-	if err != nil {
-		return err
-	}
-
-	return unmarshallYamlIntoStruct(secretData, configStruct)
-}
-
-func getSecretFromSecretManager(ctx context.Context, client *secretmanager.Client, name string) ([]byte, error) {
-	req := &secretmanagerpb.AccessSecretVersionRequest{
-		Name: name,
-	}
-
-	result, err := client.AccessSecretVersion(ctx, req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to access secret version: %v", err)
-	}
-
-	return result.Payload.Data, nil
-}
-
-func unmarshallYamlIntoStruct(data []byte, dstStruct interface{}) error {
-	err := yaml.Unmarshal(data, dstStruct)
-	return err
 }
---
+++ pkg/config/util.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"context"
+	"errors"
+	"fmt"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+func LoadConfigsFromSecretManager(ctx context.Context, client *secretmanager.Client, configStruct interface{}) error {
+	secretName, err := getSecretNameByConfigStruct(configStruct)
+	if err != nil {
+		return err
+	}
+
+	return loadConfigFromSecretManager(ctx, client, secretName, configStruct)
+}
+
+func getSecretNameByConfigStruct(configStruct interface{}) (string, error) {
+	switch configStruct.(type) {
+	case *Common:
+		return os.Getenv(CommonSecretEnvVar), nil
+	case *ManagementConfigs:
+		return os.Getenv(ManagementSecretEnvVar), nil
+	case *BackofficeConfigs:
+		return os.Getenv(BackofficeSecretEnvVar), nil
+	case *AuthConfigs:
+		return os.Getenv(AuthSecretEnvVar), nil
+	case *TranscoderConfigs:
+		return os.Getenv(TranscoderSecretEnvVar), nil
+	case *Web3EventsConfigs:
+		return os.Getenv(Web3EventsSecretEnvVar), nil
+	default:
+		return "", errors.New("unsupported configStruct")
+	}
+}
+
+func loadConfigFromSecretManager(ctx context.Context, client *secretmanager.Client, name string, configStruct interface{}) error {
+	secretData, err := getSecretFromSecretManager(ctx, client, name)
+	if err != nil {
+		return err
+	}
+
+	return unmarshallYamlIntoStruct(secretData, configStruct)
+}
+
+func getSecretFromSecretManager(ctx context.Context, client *secretmanager.Client, name string) ([]byte, error) {
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: name,
+	}
+
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to access secret version: %v", err)
+	}
+
+	return result.Payload.Data, nil
+}
+
+func unmarshallYamlIntoStruct(data []byte, dstStruct interface{}) error {
+	err := yaml.Unmarshal(data, dstStruct)
+	return err
+}
