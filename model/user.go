d979b61df118c677645d2f7cb9355c173561a17a
atyma <a.tymkiv99@gmail.com>
2022-08-18 11:22:17 +0300

    added wallet Edges to user entity

--- model/user.go
+++ model/user.go
@@ -1,21 +1,45 @@
 package model
 
-import "time"
+import (
+	"time"
+)
 
 type User struct {
-	ID                  int      `json:"id"`      // used to parse from ent model
-	UserId              int      `json:"user_id"` // used to parse from jwt token
-	Name                string   `json:"name,omitempty"`
-	Email               string   `json:"email"`
-	ProfileImageUrl     string   `json:"profile_image_url,omitempty"`
-	HasAdminPanelAccess bool     `json:"has_admin_panel_access"`
-	WalletAddresses     []string `json:"wallet_addresses"`
-	Faucet              Faucet   `json:"faucet"`
-	TwitterHandle       string   `json:"twitter_handle"`
-	IsNftCreator        bool     `json:"is_nft_creator"`
+	ID              int       `json:"id"`      // used to parse from ent model
+	UserId          int       `json:"user_id"` // used to parse from jwt token
+	Name            string    `json:"name,omitempty"`
+	Email           string    `json:"email"`
+	ProfileImageUrl string    `json:"profile_image_url,omitempty"`
+	WalletAddresses []string  `json:"wallet_addresses"`
+	Faucet          Faucet    `json:"faucet"`
+	TwitterHandle   string    `json:"twitter_handle"`
+	IsNftCreator    bool      `json:"is_nft_creator"`
+	Edges           UserEdges `json:"edges"`
+}
+
+type UserEdges struct {
+	Wallets []*Wallet `json:"wallets"`
+}
+
+type Wallet struct {
+	ID            int       `json:"id,omitempty"`
+	Name          string    `json:"name,omitempty"`
+	Description   string    `json:"description,omitempty"`
+	WalletAddress string    `json:"wallet_address,omitempty"`
+	Provider      string    `json:"provider,omitempty"`
+	CreatedAt     time.Time `json:"created_at,omitempty"`
+	UpdatedAt     time.Time `json:"updated_at,omitempty"`
 }
 
 type Faucet struct {
 	MaticAllowed bool      `json:"matic_allowed"`
 	LastUsed     time.Time `json:"last_used"`
+}
+
+func (u *User) SetWalletAddressesStringListFromEdges() {
+	u.WalletAddresses = make([]string, len(u.Edges.Wallets))
+
+	for i := range u.Edges.Wallets {
+		u.WalletAddresses[i] = u.Edges.Wallets[i].WalletAddress
+	}
 }
