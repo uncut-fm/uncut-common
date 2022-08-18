c279fc2d1df0773ff8d0a02600a8db10a2369b63
atyma <a.tymkiv99@gmail.com>
2022-08-18 17:33:46 +0300

    fix auth call to list wallets by userID

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -12,7 +12,7 @@ var (
 	getCreatorsEndpoint     = "%s/user/creators"
 	updateUserEndpoint      = "%s/user/admin"
 	getUserEndpoint         = "%s/user/"
-	getWalletsEndpoint      = "%s/user/wallet/"
+	getWalletsEndpoint      = "%s/user/wallets/"
 )
 
 const requestTimeout = 5 * time.Second
