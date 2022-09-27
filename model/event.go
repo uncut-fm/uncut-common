6b7608ad160afce102d49674f63254600e019df7
atyma <a.tymkiv99@gmail.com>
2022-09-27 14:35:56 +0300

    added NewNFTMintedEvent event

--- model/event.go
+++ model/event.go
@@ -18,4 +18,5 @@ var (
 	NewSpaceConversationCommentEvent EventType = "NewSpaceConversationComment"
 	NewSpaceConversationReplyEvent   EventType = "NewSpaceConversationReply"
 	NewCreatorEvent                  EventType = "NewCreator"
+	NewNFTMintedEvent                EventType = "NewNFTMinted"
 )
