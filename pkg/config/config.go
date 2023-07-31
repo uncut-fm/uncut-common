91e1b48f341e28c04ba47181d4e40d2418dce2b0
atyma <a.tymkiv99@gmail.com>
2023-07-31 13:36:39 +0300

    added adminToken to transcoderConfigs

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -152,6 +152,7 @@ type TranscoderConfigs struct {
 	Server           ServerConfigs     `yaml:"server"`
 	Headers          map[string]string `yaml:"headers"`
 	Sentry           SentryConfigs     `yaml:"sentry"`
+	AdminToken       string            `yaml:"admin_token"`
 }
 
 type Web3EventsConfigs struct {
