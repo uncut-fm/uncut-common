a0d76cee4a7081b30c0643e83241cdeed7c690d8
atyma <a.tymkiv99@gmail.com>
2023-09-28 15:54:17 +0300

    loading users by wallets and ids in batches in parallel if needed

--- pkg/auth/get.go
+++ pkg/auth/get.go
@@ -7,6 +7,7 @@ import (
 	proto_user "github.com/uncut-fm/uncut-common/pkg/proto/auth/user"
 	"google.golang.org/grpc/metadata"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -165,6 +166,52 @@ func (a API) SearchUsers(ctx context.Context, keyword string, pagination *model.
 }
 
 func (a API) ListUsersByWalletAddresses(ctx context.Context, walletAddresses []string, orderBy *model.UserOrder, pagination *model.OffsetPaginationInput) (*UsersInfoResponse, error) {
+	mainResponse := &UsersInfoResponse{}
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	// to collect all errors
+	var errs []error
+	var errMu sync.Mutex
+
+	const batchSize = 1000
+
+	// load in batches of 1000 to avoid grpc error "message too large"
+	// run in parallel and merge results
+	for i := 0; i < len(walletAddresses); i += batchSize {
+		wg.Add(1)
+		go func(start int) {
+			defer wg.Done()
+			end := start + batchSize
+			if end > len(walletAddresses) {
+				end = len(walletAddresses)
+			}
+
+			response, err := a.listUsersByWalletAddresses(ctx, walletAddresses[start:end], orderBy, pagination)
+			if a.log.CheckError(err, a.ListUsersByWalletAddresses) != nil {
+				errMu.Lock()
+				errs = append(errs, err)
+				errMu.Unlock()
+				return
+			}
+
+			mu.Lock()
+			mainResponse.TotalCount += response.TotalCount
+			mainResponse.Users = append(mainResponse.Users, response.Users...)
+			mu.Unlock()
+		}(i)
+	}
+
+	wg.Wait()
+
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+
+	return mainResponse, nil
+}
+
+func (a API) listUsersByWalletAddresses(ctx context.Context, walletAddresses []string, orderBy *model.UserOrder, pagination *model.OffsetPaginationInput) (*UsersInfoResponse, error) {
 	req := &proto_user.WalletAddressesRequest{WalletAddresses: walletAddresses}
 	if orderBy != nil {
 		req.Order = model.ParseUserOrderToProto(orderBy)
@@ -188,6 +235,55 @@ func (a API) ListUsersByWalletAddresses(ctx context.Context, walletAddresses []s
 }
 
 func (a API) ListUsersByIDs(ctx context.Context, userIDs []int, orderBy *model.UserOrder, pagination *model.OffsetPaginationInput) (*UsersInfoResponse, error) {
+	mainResponse := &UsersInfoResponse{}
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	// to collect all errors
+	var errs []error
+	var errMu sync.Mutex
+
+	// Decide on a reasonable batch size. Here, I'm using 1000 as an example.
+	const batchSize = 1000
+
+	// load in batches of batchSize
+	// run in parallel and merge results
+	for i := 0; i < len(userIDs); i += batchSize {
+		wg.Add(1)
+		go func(start int) {
+			defer wg.Done()
+
+			end := start + batchSize
+			if end > len(userIDs) {
+				end = len(userIDs)
+			}
+
+			partialResponse, err := a.listUsersByIDs(ctx, userIDs[start:end], orderBy, pagination)
+			if a.log.CheckError(err, a.ListUsersByWalletAddresses) != nil {
+				errMu.Lock()
+				errs = append(errs, err)
+				errMu.Unlock()
+				return
+			}
+
+			mu.Lock()
+			mainResponse.TotalCount += partialResponse.TotalCount
+			mainResponse.Users = append(mainResponse.Users, partialResponse.Users...)
+			mu.Unlock()
+		}(i)
+	}
+
+	wg.Wait()
+
+	if len(errs) > 0 {
+		// handle the errors as you see fit, for simplicity, we're just returning the first one
+		return nil, errs[0]
+	}
+
+	return mainResponse, nil
+}
+
+func (a API) listUsersByIDs(ctx context.Context, userIDs []int, orderBy *model.UserOrder, pagination *model.OffsetPaginationInput) (*UsersInfoResponse, error) {
 	req := &proto_user.IDsRequest{Ids: model.IntToUInt64Slice(userIDs)}
 	if orderBy != nil {
 		req.Order = model.ParseUserOrderToProto(orderBy)
@@ -202,12 +298,10 @@ func (a API) ListUsersByIDs(ctx context.Context, userIDs []int, orderBy *model.U
 		return nil, err
 	}
 
-	response := &UsersInfoResponse{
+	return &UsersInfoResponse{
 		TotalCount: int(protoUsersInfo.TotalCount),
 		Users:      model.ParseProtoUsersToCommonUsers(protoUsersInfo.Users),
-	}
-
-	return response, nil
+	}, nil
 }
 
 func (a API) ListWalletsByUserID(ctx context.Context, userID int) ([]*model.Wallet, error) {
