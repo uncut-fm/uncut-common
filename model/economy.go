38397c68938c2a8c17e19ec0bf67bbdbd8026989
atyma <a.tymkiv99@gmail.com>
2024-01-10 11:23:51 +0200

    update signup reward from 1000 to 500

--- model/economy.go
+++ model/economy.go
@@ -2,7 +2,7 @@ package model
 
 const (
 	CompleteProfileRewardAmount          = 1000
-	SignupRewardAmount                   = 1000
+	SignupRewardAmount                   = 500
 	SigninRewardAmount                   = 100
 	SigninAdditionalDailyRewardAmount    = 10
 	SigninMaxAdditionalDailyRewardAmount = 100
