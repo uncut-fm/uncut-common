fd32d5dbe7ae43249cab5878874ca6b2c1c3b6ed
atyma <a.tymkiv99@gmail.com>
2024-01-17 17:51:25 +0200

    updated artx rewards

--- model/economy.go
+++ model/economy.go
@@ -4,7 +4,7 @@ const (
 	CompleteProfileRewardAmount          = 500
 	SignupRewardAmount                   = 500
 	SigninRewardAmount                   = 25
-	SigninAdditionalDailyRewardAmount    = 10
-	SigninMaxAdditionalDailyRewardAmount = 100
-	SellNftRewardAmount                  = 30
+	SigninAdditionalDailyRewardAmount    = 5
+	SigninMaxAdditionalDailyRewardAmount = 75
+	//SellNftRewardAmount                  = 30
 )
