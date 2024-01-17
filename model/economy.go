e44e0d7da56af1daf44bec5d755b0988eb40fadd
atyma <a.tymkiv99@gmail.com>
2024-01-17 17:42:19 +0200

    set SigninRewardAmount to 25

--- model/economy.go
+++ model/economy.go
@@ -3,7 +3,7 @@ package model
 const (
 	CompleteProfileRewardAmount          = 500
 	SignupRewardAmount                   = 500
-	SigninRewardAmount                   = 100
+	SigninRewardAmount                   = 25
 	SigninAdditionalDailyRewardAmount    = 10
 	SigninMaxAdditionalDailyRewardAmount = 100
 	SellNftRewardAmount                  = 30
