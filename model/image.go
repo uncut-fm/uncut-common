95044c6a31187bbfaef79d530d0941b99e1c023c
atyma <a.tymkiv99@gmail.com>
2024-02-07 12:08:25 +0200

    added GifFirstFrameUrl to ImageMetadata

--- model/image.go
+++ model/image.go
@@ -1,9 +1,10 @@
 package model
 
 type ImageMetadata struct {
-	Width       int
-	Height      int
-	AspectRatio float64
-	Size        int
-	MimeType    string
+	Width            int
+	Height           int
+	AspectRatio      float64
+	Size             int
+	MimeType         string
+	GifFirstFrameUrl *string
 }
