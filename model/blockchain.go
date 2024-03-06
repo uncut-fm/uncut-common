d4a87da899b558135e1fca1fdb05b1b94090760b
atyma <a.tymkiv99@gmail.com>
2024-03-06 19:46:34 +0200

    added WaxCollectionName validation

--- model/blockchain.go
+++ model/blockchain.go
@@ -1,7 +1,9 @@
 package model
 
 import (
+	"errors"
 	"github.com/uncut-fm/uncut-common/pkg/config"
+	"strings"
 )
 
 type BlockchainNetwork string
@@ -71,4 +73,37 @@ func (w WaxCollectionInfo) GetMap() map[string]interface{} {
 		"market_fee":          w.MarketFee,
 		"allow_notify":        w.AllowNotify,
 	}
+}
+
+// Name ensures that the name is a valid eosio name
+func WaxName(str string) (string, error) {
+	str = strings.ToLower(str)
+
+	if len(str) > 12 {
+		return "", errors.New("Name too long")
+	}
+
+	charmap := ".12345abcdefghijklmnopqrstuvwxyz"
+
+	for _, c := range str {
+		if strings.IndexRune(charmap, c) == -1 {
+			return "", errors.New("Invalid character in name")
+		}
+	}
+
+	// Last character can't be a dot
+	if str[len(str)-1] == '.' {
+		return "", errors.New("Last character can't be a dot")
+	}
+
+	return str, nil
+}
+
+// CollectionName ensures that the collection name is 12 characters long and a valid eosio name
+func WaxCollectionName(str string) (string, error) {
+	if len(str) != 12 {
+		return "", errors.New("Collection name must be 12 characters while this one is " + string(len(str)))
+	}
+
+	return WaxName(str)
 }
