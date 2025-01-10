459b8cc75abcd9e3db6f4dea09428254190ed97d
atyma <a.tymkiv99@gmail.com>
2025-01-10 12:13:50 +0200

    updated ReferrerReachingLevelNBonusRewardAmount 250->1000

--- model/economy.go
+++ model/economy.go
@@ -5,23 +5,23 @@ const (
 	TOTAL_TREASURY_ID     = -1
 	NULL_TREASURY_ID      = -2
 
-	CompleteProfileRewardAmount          = 100
-	SignupRewardAmount                   = 5
-	signupGrantFromAmount                = 25
-	SignupGrantToAmount                  = 100
-	SigninRewardAmount                   = 10
-	SigninAdditionalDailyRewardAmount    = 5
-	SigninMaxAdditionalDailyRewardAmount = 15
-	NFTMintCopyFeeGeneral                = 2
-	NFTMintCopyFeeForWaxp                = 2
-	NFTMintCopyFeeForPolygonETH          = 3
-	NewCollectionFee                     = 100
-	NewCollectionFeeForWaxp              = 50
-	NewCollectionFeeForPolygon           = 100
-	NFTVoteFee                           = 5
-	PromoteCuratedNftFee                 = 200
-	ReferralSignupBonusRewardAmount      = 100
-	ReferrerBonusRewardAmount            = 250
+	CompleteProfileRewardAmount             = 100
+	SignupRewardAmount                      = 5
+	signupGrantFromAmount                   = 25
+	SignupGrantToAmount                     = 100
+	SigninRewardAmount                      = 10
+	SigninAdditionalDailyRewardAmount       = 5
+	SigninMaxAdditionalDailyRewardAmount    = 15
+	NFTMintCopyFeeGeneral                   = 2
+	NFTMintCopyFeeForWaxp                   = 2
+	NFTMintCopyFeeForPolygonETH             = 3
+	NewCollectionFee                        = 100
+	NewCollectionFeeForWaxp                 = 50
+	NewCollectionFeeForPolygon              = 100
+	NFTVoteFee                              = 5
+	PromoteCuratedNftFee                    = 200
+	ReferralSignupBonusRewardAmount         = 100
+	ReferrerReachingLevelNBonusRewardAmount = 1000
 
 	ArtxUsdRate   = 0.003 // 1 ARTX = 0.003 USD.
 	UsdToArtxRate = 333   // 1 USD = 333.33 ARTX.
